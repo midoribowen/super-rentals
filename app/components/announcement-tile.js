bc1965130541b1bd7223f126019a2b4aa56ebc88
Midori Bowen and Nathan Romike <midori.p.bowen@gmail.com>
2016-02-22 13:26:55 -0800

    Destroy an announcement.

--- app/components/announcement-tile.js
+++ app/components/announcement-tile.js
@@ -8,6 +8,11 @@ export default Ember.Component.extend({
     },
     messageHide: function() {
       this.set('isMessageShowing', false);
+    },
+    delete(announcements) {
+      if (confirm('Are you sure you want to delete this announcement?')) {
+        this.sendAction('destroyAnnouncement', announcements);
+      }
     }
   }
 });
--- app/routes/announcements.js
+++ app/routes/announcements.js
@@ -2,13 +2,18 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    return this.store.findAll('announcements')
+    return this.store.findAll('announcements');
   },
 
   actions: {
     save3(params) {
       var newAnnouncement = this.store.createRecord('announcements', params);
       newAnnouncement.save();
+      this.transitionTo('announcements');
+    },
+
+    destroyAnnouncement(announcements) {
+      announcements.destroyRecord();
       this.transitionTo('announcements');
     }
   }
